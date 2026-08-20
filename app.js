3a9018737c1221e56a7fe2eccaa43678ad0c22de
Emirhan-web <emirhan.yasar@istun.edu.tr>
2026-08-20 17:56:46 +0300

    görev eklemedeki yazım hatası düzeltildi

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-const readline = requrie("readline");
+const readline = require("readline");
 
-const r1 = readline.CreateInterFace({
+const r1 = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-r1.question("Seçiminiz: ", (secim)=>{
-    if(secim==="1"){
-        console.log("Görev Ekleme Seçildi");
-    }
-    else if(secim==="2"){
-        console.log("Görevleri Listeleme Seçildi");
-    }
-    else if(secim==="3"){
-        console.log("Program Kapatılıyor");
-    }
-    else{
-        console.log("Geçersiz İşlem");
-    }
-
-    r1.close();
-})
+const gorevler = [];
 
 
 console.log("=== Görev  Yönetim Sistemi ===")
@@ -28,5 +13,32 @@ console.log("=== Görev  Yönetim Sistemi ===")
 console.log("1- Görev Ekle");
 console.log("2- Görevleri Listele");
 console.log("3- Çıkış");
+
+
+r1.question("Seçiminiz: ", (secim)=>{
+    if(secim==="1"){
+        r1.question("Görevi girin: ", (gorev)=>{
+            gorevler.push(gorev);
+            console.log("Görev Eklendi: " + gorev);
+            r1.close();
+        });
+    }
+    else if(secim==="2"){
+        console.log("Görevleri Listeleme Seçildi");
+        r1.close();
+    }
+    else if(secim==="3"){
+        console.log("Program Kapatılıyor");
+        r1.close();
+    }
+    else{
+        console.log("Geçersiz İşlem");
+        r1.close();
+    }
+
+    
+});
+
+
 
 
