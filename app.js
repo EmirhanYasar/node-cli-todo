a02961edd3bb7cf8f8182c61efbb7ca8599647ff
Emirhan-web <emirhan.yasar@istun.edu.tr>
2026-08-22 14:32:19 +0300

    menuGosterme fonksiyonu ve görev listeleme eklendi

--- app.js
+++ app.js
@@ -8,36 +8,45 @@ const r1 = readline.createInterface({
 const gorevler = [];
 
 
-console.log("=== Görev  Yönetim Sistemi ===")
+function menuGoster() {
+    console.log("=== Görev  Yönetim Sistemi ===")
 
-console.log("1- Görev Ekle");
-console.log("2- Görevleri Listele");
-console.log("3- Çıkış");
-
-
-r1.question("Seçiminiz: ", (secim)=>{
-    if(secim==="1"){
-        r1.question("Görevi girin: ", (gorev)=>{
-            gorevler.push(gorev);
-            console.log("Görev Eklendi: " + gorev);
-            r1.close();
-        });
-    }
-    else if(secim==="2"){
-        console.log("Görevleri Listeleme Seçildi");
-        r1.close();
-    }
-    else if(secim==="3"){
-        console.log("Program Kapatılıyor");
-        r1.close();
-    }
-    else{
-        console.log("Geçersiz İşlem");
-        r1.close();
-    }
-
+    console.log("1- Görev Ekle");
+    console.log("2- Görevleri Listele");
+    console.log("3- Çıkış");
     
-});
+    r1.question("Seçiminiz. ",(secim)=>{
+        if(secim==="1"){
+            r1.question("Görevi Girin: ", (gorev)=>{
+                gorevler.push(gorev);
+                console.log("Görev Eklendi: " + gorev);
+                menuGoster();
+            })
+        }
+        else if(secim==="2"){
+            if(gorevler.length===0){
+                console.log("Henüz Görev Eklenmedi");
+            }
+            else{
+                console.log("\n=== Görevler ===");
+                gorevler.forEach((gorev,index)=>{
+                    console.log((index+1) + "-" + gorev);
+                });
+            }
+            menuGoster();
+        }
+        else if(secim==="3"){
+            console.log("Program Kapatılıyor...");
+            r1.close();
+        }
+        else{
+            console.log("Geçersiz İşlem");
+            menuGoster();
+        }
+
+    });
+}
+menuGoster();
 
 
 
